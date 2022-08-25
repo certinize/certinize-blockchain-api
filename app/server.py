4ee38641611985c4ad5f6a9443d339e594491868
Hju Kneyck Flores <hjucode@gmail.com>
2022-08-25 13:52:30 +0800

    refactor: use custom bindings for validation

--- app/server.py
+++ app/server.py
@@ -6,27 +6,30 @@ This module contains the server startup logic.
 """
 import asyncio
 import platform
-import typing
 
 import blacksheep
 import uvloop
 
-from app import dependencies, errors, middlewares
+from app import bindings, dependencies, errors, middlewares, models
 from app import settings as settings
 
 if platform.system() == "Linux":
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 app = blacksheep.Application()
+
+# Register services
 app.services.add_exact_scoped(dependencies.Validator)  # type: ignore
+
+
+# Exception handlers
 app.exceptions_handlers[errors.BadRequest] = errors.error_400_handler  # type: ignore
+
+# Middlewares
 app.middlewares.append(middlewares.MediaTypeValidator())
 
-get = app.router.get
-post = app.router.post
 
-
-@get("/")
+@app.router.get("/")
 async def index() -> dict[str, str]:
     return {
         "message": "Hello, World!",
@@ -34,12 +37,8 @@ async def index() -> dict[str, str]:
     }
 
 
-@post("/issuances")
+@app.router.post("/issuances")
 async def issue_certificate(
-    request: blacksheep.Request, test: dependencies.Validator
-) -> dict[str, typing.Any]:
-
-    request = await test.validate_issuance_request(request)
-    request_body = await request.json()
-
-    return request_body
+    data: bindings.FromIssuanceRequestModel[models.IssuanceRequest],
+) -> models.IssuanceRequest:
+    return data.value
